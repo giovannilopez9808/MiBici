477750112e01dbf60910a93fee065ed9d3dd1857
Giovanni Lopez <giovannilopez9808@gmail.com>
2021-11-13 22:14:03 -0600

    Add daily-hour mean

---
+++ Scripts/Graphics_daily_and_hour_mean.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+from Functions import *
+from pylab import *
+
+
+def set_day_of_the_week(data: DataFrame, parameters: dict) -> DataFrame:
+    data["Day"] = None
+    for index in data.index:
+        date = data["Date"][index]
+        day = date.strftime("%A")
+        day_number = parameters["days"][day]
+        data.loc[index, "Day"] = day_number
+    return data
+
+
+parameters = {"path data": "../Output/",
+              "file data": "Hourly_count.csv",
+              "days": {"lunes": "1",
+                       "martes": "2",
+                       "miércoles": "3",
+                       "jueves": "4",
+                       "viernes": "5",
+                       "sábado": "6",
+                       "domingo": "7"}}
+
+data = read_data(parameters["path data"],
+                 parameters["file data"])
+data["Date"] = pd.to_datetime(data["Date"])
+set_day_of_the_week(data,
+                    parameters)
+data_mean = data.groupby("Day").mean()
+data_mean = data_mean.sort_index()
+xticks = np.arange(-0.5, 23.5-6+2, 2)
+yticks = np.linspace(-0.5, 6.5, 8)
+yticks = np.delete(yticks, -1)
+xlabels = xticks+0.5+6
+xlabels = [int(xlabel) for xlabel in xlabels]
+data_mean = data_mean.drop(columns=[str(i) for i in range(6)])
+data_mean = data_mean.drop(columns="23")
+data_mean = np.array(data_mean)
+cmap = cm.get_cmap('Greens', 9)
+plt.xticks(xticks,
+           xlabels)
+plt.yticks(yticks,
+           parameters["days"])
+plt.grid(ls="--",
+         color="#000000",
+         alpha=0.5,
+         lw=1.5)
+plt.imshow(data_mean,
+           origin="lower",
+           cmap=cmap,
+           vmin=0,
+           vmax=100,
+           aspect='auto')
+# plt.contourf(data_mean,
+#              origin="lower",
+#              cmap="Greens")
+plt.tight_layout()
+plt.colorbar(ticks=np.linspace(0, 100, 11))
+plt.show()
--- Scripts/Graphics_monthly_and_hourly_mean.py
+++ Scripts/Graphics_monthly_and_hourly_mean.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from Functions import *
-
+from pylab import *
 
 parameters = {"path data": "../Output/",
               "file data": "Hourly_count.csv"}
@@ -11,26 +11,32 @@ data["Date"] = pd.to_datetime(data["Date"])
 data["Date"] = data["Date"].apply(lambda x: x.replace(year=2016))
 data.index = pd.to_datetime(data["Date"])
 data_mean = data.resample("MS").mean()
-xticks = np.arange(0.5, 23.5-6+2, 2)
-yticks = np.linspace(0.5, 11.5, 13)
-yticks = np.delete(yticks, 12)
-xlabels = xticks-0.5+6
+xticks = np.arange(-0.5, 23.5-6+2, 2)
+yticks = np.linspace(-0.5, 11.5, 13)
+yticks = np.delete(yticks, -1)
+xlabels = xticks+0.5+6
 xlabels = [int(xlabel) for xlabel in xlabels]
 ylabels = ["Ene", "Feb", "Mar", "Abril", "May",
            "Jun", "Jul", "Ago", "Sept", "Oct", "Nov", "Dec"]
 data_mean = data_mean.drop(columns=[str(i) for i in range(6)])
 data_mean = data_mean.drop(columns="23")
 data_mean = np.array(data_mean)
+cmap = cm.get_cmap('Blues', 8)
 plt.xticks(xticks,
            xlabels)
 plt.yticks(yticks,
            ylabels)
 plt.grid(ls="--",
-         color="#000000",
+         color="#ffffff",
          alpha=0.5,
          lw=1.5)
-plt.contourf(data_mean,
-             origin="lower",
-             cmap="Greens")
-plt.colorbar()
+plt.imshow(data_mean,
+           origin="lower",
+           cmap=cmap,
+           vmin=0,
+           vmax=80,
+           interpolation='spline36',
+           aspect="auto")
+plt.colorbar(ticks=np.linspace(0, 80, 9))
+plt.tight_layout()
 plt.show()
