a6d6f45ad6640551af4f86e02ff6277ff7ba8c71
Giovanni Lopez <giovannilopez9808@gmail.com>
2021-11-27 20:09:03 -0600

    Add colorbar label

--- Scripts/Graphics_monthly_and_hourly_mean_count.py
+++ Scripts/Graphics_monthly_and_hourly_mean_count.py
@@ -3,7 +3,9 @@ from Functions import *
 from pylab import *
 
 parameters = {"path data": "../Output/",
-              "file data": "Hourly_count_travel.csv"}
+              "file data": "Hourly_count_travel.csv",
+              "path graphics": "../Graphics/",
+              "file graphics": "monthly_and_hourly.png"}
 
 data = read_data(parameters["path data"],
                  parameters["file data"])
@@ -24,20 +26,32 @@ data_mean = np.array(data_mean)
 cmap = cm.get_cmap('Blues', 8)
 plt.xticks(xticks,
            xlabels)
-plt.yticks(yticks,
+plt.yticks(yticks+0.5,
            ylabels)
 plt.xlabel("Hora local (h)")
 plt.grid(ls="--",
          color="#ffffff",
          alpha=0.5,
-         lw=1.5)
+         lw=1.5,
+         axis="x")
+for ytick in yticks:
+    plt.plot([xticks[0], 16.5],
+             [ytick, ytick],
+             ls="--",
+             color="#ffffff",
+             alpha=0.5,
+             lw=1.5)
 plt.imshow(data_mean,
            origin="lower",
            cmap=cmap,
            vmin=0,
            vmax=80,
            aspect="auto")
-plt.colorbar(ticks=np.linspace(0, 80, 9))
+cbar = plt.colorbar(ticks=np.linspace(0, 80, 9))
+cbar.set_label("Promedio de viajes",
+               rotation=-90,
+               labelpad=15)
 plt.tight_layout()
-plt.savefig("../Graphics/monthly_and_hourly_mean_count.png",
+plt.savefig("{}{}".format(parameters["path graphics"],
+                          parameters["file graphics"]),
             dpi=400)
