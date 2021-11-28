db17e618d52a3c9bf76a6f787b962cca37164b1d
Giovanni Lopez <giovannilopez9808@gmail.com>
2021-11-27 23:11:52 -0600

    Basis to monthly and hourly mean distance graphics

---
+++ Scripts/Graphics_monthly_and_hourly_mean_distance.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+from Functions import *
+from pylab import *
+
+parameters = {"path data": "../Output/",
+              "file data": "Hourly_mean_time_travel.csv",
+              "path graphics": "../Graphics/",
+              "file graphics": "monthly_hourly_mean_time_travel.png"}
+
+data = read_data(parameters["path data"],
+                 parameters["file data"])
+data["Date"] = pd.to_datetime(data["Date"])
+data["Date"] = data["Date"].apply(lambda x: x.replace(year=2016))
+data.index = pd.to_datetime(data["Date"])
+data_mean = data.resample("MS").mean()
+xticks = np.arange(-0.5, 23.5-6+2, 2)
+yticks = np.linspace(-0.5, 11.5, 13)
+yticks = np.delete(yticks, -1)
+xlabels = xticks+0.5+6
+xlabels = [int(xlabel) for xlabel in xlabels]
+ylabels = ["Ene", "Feb", "Mar", "Abril", "May",
+           "Jun", "Jul", "Ago", "Sept", "Oct", "Nov", "Dec"]
+data_mean = data_mean.drop(columns=[str(i) for i in range(6)])
+data_mean = data_mean.drop(columns="23")
+data_mean = np.array(data_mean)
+cmap = cm.get_cmap('magma', 10)
+plt.xticks(xticks,
+           xlabels)
+plt.yticks(yticks+0.5,
+           ylabels)
+plt.xlabel("Hora local (h)")
+plt.grid(ls="--",
+         color="#ffffff",
+         alpha=0.5,
+         lw=1.5,
+         axis="x")
+plt.imshow(data_mean,
+           origin="lower",
+           cmap=cmap,
+           vmin=0,
+           vmax=100,
+           aspect="auto")
+for ytick in yticks:
+    plt.plot([xticks[0], 16.5],
+             [ytick, ytick],
+             ls="--",
+             color="#ffffff",
+             alpha=0.5,
+             lw=1.5)
+cbar = plt.colorbar(ticks=np.linspace(0, 100, 11))
+cbar.set_label("Distancia promedio por viaje (min)",
+               rotation=-90,
+               labelpad=15)
+plt.tight_layout()
+plt.savefig("{}{}".format(parameters["path graphics"],
+                          parameters["file graphics"]),
+            dpi=400)
