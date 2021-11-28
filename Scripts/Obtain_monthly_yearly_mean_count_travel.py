2026ffc34cce656e6e18c894aa4962b2d2d0716e
Giovanni Lopez <giovannilopez9808@gmail.com>
2021-11-27 21:19:27 -0600

    Ommit graphic  and obtain monthly yearly mean

---
+++ Scripts/Obtain_monthly_yearly_mean_count_travel.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from Functions import *
+
+parameters = {"path data": "../Output/",
+              "path graphics": "../Graphics/",
+              "file data": "Daily_count_travel.csv",
+              "file monthly output": "Monthly_mean_count_travel.csv",
+              "file yearly output": "Yearly_mean_count_travel.csv"}
+# Lectura de los datos
+data = pd.read_csv("{}{}".format(parameters["path data"],
+                                 parameters["file data"]))
+# Formato de los datos
+data = format_data(data)
+# Calculo del promedio mensual
+monthly_mean = data.resample("MS").mean()
+yearly_mean = data.resample("Y").mean()
+data.to_csv("{}{}".format(parameters["path data"],
+                          parameters["file monthly output"]))
+data.to_csv("{}{}".format(parameters["path data"],
+                          parameters["file yearly output"]))
