ec359647b42bb5ae36599500c121edca85c9747c
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-11-28 19:03:26 -0600

    Monthly and yearly mean time and distance

---
+++ Scripts/Obtain_monthly_yearly_mean_distance_travel.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from Functions import *
+
+parameters = {"path data": "../Output/",
+              "path graphics": "../Graphics/",
+              "file data": "Daily_mean_distance.csv",
+              "file monthly output": "Monthly_mean_distance.csv",
+              "file yearly output": "Yearly_mean_distance.csv"}
+# Lectura de los datos
+data = pd.read_csv("{}{}".format(parameters["path data"],
+                                 parameters["file data"]))
+# Formato de los datos
+data = format_data(data)
+# Calculo del promedio mensual
+monthly_mean = data.resample("MS").mean()
+yearly_mean = data.resample("Y").mean()
+data.to_csv("{}{}".format(parameters["path data"],
+                          parameters["file monthly output"]))
+data.to_csv("{}{}".format(parameters["path data"],
+                          parameters["file yearly output"]))
