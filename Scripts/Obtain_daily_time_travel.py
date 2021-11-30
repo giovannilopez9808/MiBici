bcc2b90b0960ded67e9f4abce186e6a3a6b04362
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-11-30 01:27:12 -0600

    Add variance per day

--- Scripts/Obtain_daily_time_travel.py
+++ Scripts/Obtain_daily_time_travel.py
@@ -1,42 +1,37 @@
 from Functions import *
 
+
 parameters = {"path output": "../Output/",
               "path data": "../Data/",
-              "path information": "../Information/",
-              "file distances": "Distance_between.csv",
-              "file output": "Daily_distance.csv",
+              "file output": "Daily_time_travel.csv",
               "useless columns": ["Usuario_Id",
                                   "Genero",
                                   "Viaje_Id",
+                                  "Origen_Id",
+                                  "Destino_Id",
                                   "Año_de_nacimiento"]
               }
-
 files = obtain_filenames(parameters["path data"])
 period = obtain_period_from_filenames(files)
 dates = obtain_consecutive_dates_from_period(period)
 daily_data_output = create_daily_dataframe(dates,
                                            header=["Mean", "Var"],
                                            use_float=True)
-distance_data = read_data(parameters["path output"],
-                          parameters["file distances"],
-                          use_index=True)
 for file in files:
     print("Analizando archivo {}".format(file))
     data = read_data(parameters["path data"],
                      file)
-    distances = distance_algorithm(data,
-                                   parameters["useless columns"],
-                                   distance_data,
-                                   distance_data.index)
-    data = distances.data
+    times = time_algorithm(data,
+                           parameters["useless columns"])
+    data = times.data
     daily_mean_data = data.resample("D").mean()
     daily_var_data = data.resample("D").var()
-    del data
+    del data, times
     for date in daily_mean_data.index:
         index = date.date()
-        value = daily_mean_data["Distance"][date]
+        value = daily_mean_data["Minutes"][date]
         daily_data_output.loc[index, "Mean"] = value
-        value = daily_var_data["Distance"][date]
+        value = daily_var_data["Minutes"][date]
         daily_data_output.loc[index, "Var"] = value
     del daily_mean_data, daily_var_data
 daily_data_output.to_csv("{}{}".format(parameters["path output"],
