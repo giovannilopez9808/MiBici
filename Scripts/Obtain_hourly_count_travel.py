0f1109e9f40de6ba2ccf78fefbee266bb3bbb43f
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-11-30 01:25:02 -0600

    Narrow down time analysis

--- Scripts/Obtain_hourly_count_travel.py
+++ Scripts/Obtain_hourly_count_travel.py
@@ -1,14 +1,6 @@
 from Functions import *
 
 
-def create_hourly_dataframe(index: list) -> DataFrame:
-    hours = [hour for hour in range(24)]
-    data = pd.DataFrame(index=index,
-                        columns=hours)
-    data = data.fillna(0.0)
-    return data
-
-
 def format_data(data: DataFrame, columns: list, date_column: str) -> DataFrame:
     data.index = pd.to_datetime(data[date_column])
     data = data.drop(columns=columns)
@@ -29,7 +21,8 @@ parameters = {"path data": "../Data/",
 files = obtain_filenames(parameters["path data"])
 period = obtain_period_from_filenames(files)
 dates = obtain_consecutive_dates_from_period(period)
-hourly_count_origin = create_hourly_dataframe(dates)
+hourly_count = create_hourly_dataframe(dates,
+                                       use_float=False)
 for file in files:
     print("Analizando archivo {}".format(file))
     data = read_data(parameters["path data"],
@@ -38,11 +31,13 @@ for file in files:
                        parameters["useless columns"],
                        "Inicio_del_viaje")
     hourly_data = data.resample("H").count()
+    del data
     for index in hourly_data.index:
         value = hourly_data["Origen_Id"][index]
         hour = index.time().hour
         date = index.date()
-        hourly_count_origin[hour][date] = value
-hourly_count_origin.index.names = ["Date"]
-hourly_count_origin.to_csv("{}{}".format(parameters["path output"],
-                                         parameters["file output"]))
+        hourly_count.loc[date, hour] = value
+    del hourly_data
+hourly_count.index.names = ["Date"]
+hourly_count.to_csv("{}{}".format(parameters["path output"],
+                                  parameters["file output"]))
