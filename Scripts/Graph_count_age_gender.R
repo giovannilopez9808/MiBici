45e894599e9fab372e2b11e7fd37b7e2d916f29a
Giovanni Lopez <giovannilopez9808@gmail.com>
2021-12-01 23:57:57 -0600

    Omite print means and add legend of lines

--- Scripts/Graph_count_age_gender.R
+++ Scripts/Graph_count_age_gender.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(latex2exp)
 path_data <- "../Output/"
 file_data <- "Age_gender_users.csv"
 path_graphics <- "../Graphics/"
@@ -7,12 +8,18 @@ dframe <- read.csv(paste(path_data,
     file_data,
     sep = ""
 ))
+colors <- c(
+    "Female mean" = "#386641",
+    "Male mean" = "#168aad"
+)
 # Edades
 f_age <- unlist(dframe[dframe$Genero == "F", "Edad"])
 m_age <- unlist(dframe[dframe$Genero == "M", "Edad"])
 # media
 f_mean <- mean(f_age)
 m_mean <- mean(m_age)
+message("Edad promedio femenino ", f_mean)
+message("Edad promedio masculino ", m_mean)
 # Graficacion con ggplot
 rm(f_age)
 rm(m_age)
@@ -24,16 +31,20 @@ p <- ggplot(
         alpha = 0.2
     ) +
     # M
-    geom_vline(aes(xintercept = m_mean),
-        color = "blue",
-        linetype = "dashed",
-        size = 0.9
+    geom_vline(aes(
+        xintercept = m_mean,
+        color = "Male mean",
+    ),
+    linetype = "dashed",
+    size = 0.9
     ) +
     # F
-    geom_vline(aes(xintercept = f_mean),
-        color = "red",
-        linetype = "dashed",
-        size = 0.9
+    geom_vline(aes(
+        xintercept = f_mean,
+        color = "Female mean",
+    ),
+    linetype = "dashed",
+    size = 0.9
     ) +
     ##
     scale_y_continuous(
@@ -42,6 +53,12 @@ p <- ggplot(
     scale_x_continuous(
         breaks = seq(16, 80, 2),
         limits = c(16, 80)
+    ) +
+    scale_fill_manual(
+        values = c(
+            "#b5179e",
+            "#4361ee"
+        )
     ) +
     theme(
         panel.background = element_rect(
@@ -53,23 +70,24 @@ p <- ggplot(
             linetype = "dashed"
         )
     ) +
-    annotate("text",
-        x = f_mean + 10,
-        y = 1700,
-        label = paste("F_mean =", round(f_mean, 2))
-    ) +
-    annotate("text",
-        x = m_mean + 10,
-        y = 1900,
-        label = paste("M_mean =", round(m_mean, 2))
-    ) +
+    # annotate("text",
+    #     x = f_mean + 10,
+    #     y = 1700,
+    #     label = TeX(paste("$\\mu_F =$", round(f_mean, 2)))
+    # ) +
+    # annotate("text",
+    #     x = m_mean + 10,
+    #     y = 1900,
+    #     label = TeX(paste("$\\mu_M =$", round(m_mean, 2)))
+    # ) +
     labs(
         title = paste(
             "Número de usuarios por edad y género en el periodo 2015-2018"
         ),
         x = "Edad",
         y = "Número de usuarios"
-    )
+    ) +
+    scale_color_manual(values = colors)
 ggsave(paste(
     path_graphics,
     file_graphics,
