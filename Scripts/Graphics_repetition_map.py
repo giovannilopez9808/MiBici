8601702d27005ff9a2c4909f9b00b40b4fc2a30b
giovannilopez9808 <giovannilopez9808@gmail.com>
2021-12-03 02:00:28 -0600

    Fix scale size of stations distribution

--- Scripts/Graphics_repetition_map.py
+++ Scripts/Graphics_repetition_map.py
@@ -8,8 +8,8 @@ def normalize_data(data: DataFrame, columns: list) -> DataFrame:
     Normalizacion de los datos entre 0 y 1
     """
     for column in columns:
-        max = data[column].max()
-        data[column] = data[column]/max
+        total = data[column].sum()
+        data[column] = data[column]/total
     return data
 
 
@@ -25,7 +25,7 @@ def plot(stations_data: DataFrame, data: DataFrame, column: str, path: str, grap
     """ 
     Realiza el ploteo del uso de las estaciones en un mapa, si se quiere realzar un zoom en la zona de interes debera dar un True en la variable limits.
     """
-    plt.subplots(figsize=(9, 4))
+    plt.subplots(figsize=(9, 5))
     # Si se quiere analizar la zona de interes
     if(limits):
         # data = filter_data(data, column)
@@ -33,22 +33,26 @@ def plot(stations_data: DataFrame, data: DataFrame, column: str, path: str, grap
         plt.ylim(150*4, 280*4)
     plt.axis("off")
     # Realiza el ploeto de los puntos con su color dependiento de su porcentaje de uso
+    cmap = cm.get_cmap('inferno', 6)
+    s = 5
+    if "zoom" in graphic_name:
+        s = 20
     points = plt.scatter(stations_data["longitude"][data.index],
                          stations_data["latitude"][data.index],
                          alpha=0.85,
-                         s=10,
-                         c=data[column],
+                         s=s,
+                         c=np.round(data[column], 3),
                          cmap=cmap)
     #  Impresion de los mapas
     plt.imshow(map.img,
-               origin="lower",
-               vmin=0,
-               vmax=1)
+               origin="lower")
     # Impresion del colorbar
-    plt.colorbar(points,
-                 ticks=np.arange(0.1,
-                                 1.1,
-                                 0.1))
+    cbar = plt.colorbar(points,
+                        ticks=np.round(np.linspace(0, 0.024, 7), 3))
+    cbar.set_label("Distribución de uso de las estaciones",
+                   rotation=-90,
+                   fontsize=13,
+                   labelpad=20)
     plt.tight_layout()
     # Guardado de la grafica
     plt.savefig("{}{}".format(path,
@@ -60,11 +64,11 @@ def plot(stations_data: DataFrame, data: DataFrame, column: str, path: str, grap
 parameters = {"path map": "../Graphics/",
               "path data": "../Output/",
               "path information": "../Information/",
-              "file map": "map_zoom.jpg",
+              "file map": "map.jpg",
               "file data": "Repetition_station.csv",
               "file coordinates": "coordinates.csv",
               "file information": "nomenclatura.csv",
-              "count columns": ["Origen_Id", "Destino_Id"],
+              "count columns": ["Origen_Id"],
               "information columns": ["latitude", "longitude"]}
 # Lectura del mapa en una clase
 map = Map_class(parameters["path map"],
@@ -87,7 +91,6 @@ stations_data = transform_coordinates_to_pixel_location(map,
 count_data = normalize_data(count_data,
                             parameters["count columns"])
 # Definicion del colormap
-cmap = cm.get_cmap('inferno', 10)
 for column in parameters["count columns"]:
     # Nombre de la grafica sin zoom
     graphic_name = "repetition_{}.png".format(
