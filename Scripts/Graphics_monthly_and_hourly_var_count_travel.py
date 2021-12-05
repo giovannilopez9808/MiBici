d6bce8387979ff6606216f414bed393632c8325a
Giovanni Lopez <giovannilopez9808@gmail.com>
2021-12-04 18:33:19 -0600

    Add monthly and hourly graphics for count travels

--- Scripts/Graphics_monthly_and_hourly_var_count_travel.py
+++ Scripts/Graphics_monthly_and_hourly_var_count_travel.py
@@ -5,14 +5,14 @@ from pylab import *
 parameters = {"path data": "../Output/",
               "file data": "Hourly_count_travel.csv",
               "path graphics": "../Graphics/",
-              "file graphics": "monthly_and_hourly_mean_count_travel.png"}
+              "file graphics": "monthly_and_hourly_var_count_travel.png"}
 
 data = read_data(parameters["path data"],
                  parameters["file data"])
 data["Date"] = pd.to_datetime(data["Date"])
 data["Date"] = data["Date"].apply(lambda x: x.replace(year=2016))
 data.index = pd.to_datetime(data["Date"])
-data_mean = data.resample("MS").mean()
+data_mean = data.resample("MS").var()
 xticks = np.arange(-0.5, 23.5-6+2, 2)
 yticks = np.linspace(-0.5, 11.5, 13)
 yticks = np.delete(yticks, -1)
@@ -23,6 +23,9 @@ ylabels = ["Ene", "Feb", "Mar", "Abril", "May",
 data_mean = data_mean.drop(columns=[str(i) for i in range(6)])
 data_mean = data_mean.drop(columns="23")
 data_mean = np.array(data_mean)
+data_mean = np.sqrt(data_mean)
+print(np.min(data_mean))
+print(np.max(data_mean))
 cmap = cm.get_cmap('magma', 8)
 plt.xticks(xticks,
            xlabels)
@@ -44,13 +47,13 @@ for ytick in yticks:
 plt.imshow(data_mean,
            origin="lower",
            cmap=cmap,
-           vmin=0,
-           vmax=80,
+           vmin=8,
+           vmax=120,
            aspect="auto")
-cbar = plt.colorbar(ticks=np.linspace(0, 80, 9))
-cbar.set_label("Promedio de los viajes realizados",
+cbar = plt.colorbar(ticks=np.linspace(0, 120, 9))
+cbar.set_label("Desviación total del los viajes realizados",
                rotation=-90,
-               labelpad=20)
+               labelpad=15)
 plt.tight_layout()
 plt.savefig("{}{}".format(parameters["path graphics"],
                           parameters["file graphics"]),
